cb6ad690a8aeb6a9ae63557f72e14dfd9944e0b6
ulisse91 <fedo.coro@gmail.com>
2015-03-20 17:58:05 +0100

    test 2nd commit

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from random import randint
 
-def creoMondo(numeroBlocchi):
+numeroBlocchi = 4 #da mettere poi random, iniziamo con 4 per vedere se funziona
+
+def creoMondo():
 	mondoTemp = []
 	for x in xrange(0,numeroBlocchi):
 		mondoTemp.append([(randint(1,9), randint(1,20))]) # coppia p_i (peso), M_i (quanto puo' reggere)
 	return mondoTemp
 
 if __name__ == '__main__':
-	mondo = creoMondo(4) #da mettere poi random, iniziamo con 4 per vedere se funziona
+	mondo = creoMondo() 
+	for x in xrange(1,10):
+		pass
 	print mondo

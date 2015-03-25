41e937d6b14afc1873bc58ccd1a82583d066bd98
LorenzoPaciotti <lorenzo.paciotti@gmail.com>
2015-03-25 18:22:43 +0100

    UCS

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from funzioniSupportoMondoBlocchi import *
 from dfs import solve_dfs
 from bfs import solve_bfs
 from deepening import solve_deepening
+from uniform import solve_ucs
 
 numeroBlocchi = 4 #da mettere poi random, iniziamo con 4 per vedere se funzion4a
 
@@ -9,16 +10,19 @@ if __name__ == '__main__':
 	tavolo = creaTavolo(numeroBlocchi)
 	print "tavolo iniziale: ", tavolo
 	goal = creaGoal(deepcopy(tavolo), numeroBlocchi)
-	print "Gaol finale: ", goal
+	print "goal finale: ", goal
 	mondo = {'tavolo': tavolo, 'braccioSx': (), 'braccioDx': ()}
 	print 'mondo: ', mondo
 	print
 	print "TEST DFS"
-	solve_dfs(mondo, goal, [putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
+	#solve_dfs(mondo, goal, [putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
 	print "TEST BFS"
-	solve_bfs(mondo, goal, [putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
+	#solve_bfs(mondo, goal, [putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
 	print "TEST DEEPENING"
-	solve_deepening(mondo, goal, [putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
+	#solve_deepening(mondo, goal, [putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
+	print "TEST UCS"
+	costi = (8,8,4,4,2,2)#COSTI(putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx)
+	solve_ucs(mondo,goal,costi,[putOnSx, putOnDx, afferraDx, afferraSx, putDownSx, putDownDx])
 
 # uno stato e' formato da {'tavolo': #lista di liste di ogni posizione del tavolo#,
 #						   'braccioSx': #elemento presente sul braccio sinistro#,
---
+++ uniform.py
@@ -0,0 +1,48 @@
+from funzioniSupportoMondoBlocchi import checkFinito
+def sort(queue):
+    for i in range(0,len(queue)-1):
+        for j in range(0,len(queue)-i-1):
+            if queue[j][1]>queue[j+1][1]:
+                temp=queue[j]
+                queue[j]=queue[j+1]
+                queue[j+1]=temp
+
+def solve_ucs(mondo,goal,costi,funzioniSuccessori):
+    queue=[([mondo],0)]
+    visited=[]
+    c_gen=1
+    c_vis=0
+    c_depth=0
+    while True:
+        if len(queue)==0:
+            print "UCS, nessuna soluzione"
+            print "UCS, numero di nodi generati: "+str(c_gen)
+            print "UCS, numero di nodi visitati: "+str(c_vis)
+            print "UCS, profondita max raggiunta: "+str(c_depth)
+            return []
+        else:
+			fringe=queue[0] #devo portarmi dietro i costi accumulati
+			queue=queue[1:] # toglie dalla lista il primo
+			head=fringe[0][0] #mod
+			c_vis+=1
+			c_depth=max(c_depth,len(fringe))
+			visited.append(head)
+			if checkFinito(head, goal):
+				print "UCS - SOLUZIONE: "
+				print fringe[0][::-1]
+				print "UCS - Generated Nodes: ", c_gen
+				print "UCS - Visited Nodes: ", c_vis
+				print "UCS - Max reached depth: ", c_depth
+				return fringe
+			else:
+				i=0
+				for function in funzioniSuccessori:
+					nodiSuccessori = function(head)
+					if nodiSuccessori != False: # se uguale a False quella mossa non era possibile
+						for successore in nodiSuccessori:
+							if successore not in visited:
+								queue = [ ( [successore] + fringe[0], costi[i]+fringe[1] ) ] + queue
+						c_gen+=len(nodiSuccessori)
+					i+=1
+			sort(queue)
+            #queue=[([temp]+fringe[0],costs[0]+fringe[1])]+queue
